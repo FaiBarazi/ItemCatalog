208b9256afc81b7ebeb64eb71be0724d4b7f125a
vagrant <vagrant@vagrant.vm>
2017-08-14 21:07:52 +0000

    Style: Adjust style to match with pep8

--- database_setup.py
+++ database_setup.py
@@ -1,27 +1,28 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer,String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-# Create movie Genre SQL Table 
+# Create movie Genre SQL Table
 class Genre(Base):
     __tablename__ = 'genre'
 
-    id = Column(Integer,primary_key=True)
-    name = Column(String,nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
 
 # Create movies SQL Table
 class Movie(Base):
     __tablename__ = 'movie'
 
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    description = Column(String(250),nullable=False)
-    genre_id = Column(Integer,ForeignKey('genre.id'))
+    description = Column(String(250), nullable=False)
+    genre_id = Column(Integer, ForeignKey('genre.id'))
 
 engine = create_engine('sqlite:///movieGenre.db')
 Base.metadata.create_all(engine)
+
