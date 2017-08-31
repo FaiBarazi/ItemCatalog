e666f941b735bad6b2f3f7c33f8b6689361dad3d
FaiBarazi <f.barazi@gmail.com>
2017-08-31 12:49:05 -0400

    style: Update to match pep8

--- database_setup.py
+++ database_setup.py
@@ -8,31 +8,36 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 # Create User Table
-class User(Base): 
-  __tablename__ = 'user'
-  id = Column(Integer, primary_key = True)
-  name = Column(String(250), nullable = False)
-  email = Column(String(250), nullable = False)
-  picture = Column(String(250))
- 
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 
 # Create movie Genre SQL Table
 class Genre(Base):
-  
+
     __tablename__ = 'genre'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    #  Return Jasonifiable object
 
-    @property #  Return Jasonifiable object 
+    @property
     def serialize(self):
-      return {
-          'name': self.name,
-          'id': self.id,
-      }
+        return {
+                'name': self.name,
+                'id': self.id,
+            }
 
 # Create movies SQL Table
+
+
 class Movie(Base):
     __tablename__ = 'movie'
     id = Column(Integer, primary_key=True)
@@ -43,13 +48,14 @@ class Movie(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
-    @property #  Return Jasonifiable object 
+    #  Return Jasonifiable object
+
+    @property
     def serialize(self):
-      return {
-          'name': self.name,
-          'description': self.description,
-          'id': self.id,
-      }
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            }
 engine = create_engine('sqlite:///movieGenre.db')
 Base.metadata.create_all(engine)
-
